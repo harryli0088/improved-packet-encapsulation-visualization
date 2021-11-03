9e8c728f3f0d3a8e4c819a6dbb4757e3b40a2c29
harryli0088 <harryli0088@gmail.com>
2021-11-03 14:56:03 -0400

    fixed repo name

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,7 @@ const config = {
       fallback: null
     }),
     paths: {
-      base: process.env.NODE_ENV==="production" ? '/your-repo-name' : undefined,
+      base: process.env.NODE_ENV==="production" ? '/improved-packet-encapsulation-visualization' : undefined,
     },
     // hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
